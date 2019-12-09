aa73807efd93c3132f765db5b60ee7be7731c1ad
jedi_knight <zhuyudong@aliyun.com>
2019-12-09 17:34:18 +0800

    docs: 丰富css内容

---
+++ src/pages/components/css/media/index.js
@@ -0,0 +1,10 @@
+import createComponent from '@src/utils/createComponent'
+import styled from 'styled-components'
+
+export default createComponent({
+  id: 'css/media',
+  examples: [],
+  dependencies: {
+    styled
+  }
+})
---
+++ src/pages/components/css/text/index.js
@@ -0,0 +1,10 @@
+import createComponent from '@src/utils/createComponent'
+import styled from 'styled-components'
+
+export default createComponent({
+  id: 'css/text',
+  examples: [],
+  dependencies: {
+    styled
+  }
+})
